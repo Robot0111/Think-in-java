b52135ba80a4932d3a08971fe9df2aa3e41bfe84
Robot0111 <desireluqilai@outlook.com>
2020-04-13 07:37:27 +0800

    转型与警告

---
+++ src/main/java/com/think/generic/UseList.java
@@ -0,0 +1,12 @@
+package com.think.generic;
+
+/**
+ * @author luQl
+ * 由于擦除的原因不能被重载
+ * @param <W>
+ * @param <T>
+ */
+public class UseList< W,T> {
+//    void f(List<T> v) {}
+//    void f(List<W> v) {}
+}
---
+++ src/main/java/com/think/generic/Widget.java
@@ -0,0 +1,5 @@
+package com.think.generic;
+
+public class Widget {
+
+}
